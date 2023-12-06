c7fa424e4cad6f106cbaaa52457e66859cf5995e
dltmdska <dltmdska@naver.com>
2023-12-06 14:25:02 +0900

    진짜 재수정 완료

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -6,46 +6,50 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 const Port = () => {
     
-    const horizontalRef = useRef([])
-    const sectionsRef = useRef([]) 
+    const horizontalRef = useRef(null)
+    const sectionRef = useRef([]) 
 
     useEffect(() =>{
-        gsap.registerPlugin(ScrollTrigger)
+        gsap.registerPlugin(ScrollTrigger);
 
-        
-        const horizontal = horizontalRef.current
-        const sections = sectionsRef.current
+        const horizontal = horizontalRef.current;
+        const sections = sectionRef.current;
 
         let scrollTween = gsap.to(sections, {
-            xPercent: -120 * (sections.length -1),
-            ease:'none',
-            scrollTrigger:{
-                trigger:horizontal,
-                start:'top 56px',
-                end:() =>'+='+horizontal.offsetWidth,
-                pin:true,
-                scrub:1,
-                invalidateOnRefresh:true,
-                anticipatePin:1
+            xPercent: -120 * (sections.length - 1),
+            ease: "none",
+            scrollTrigger: {
+                trigger: horizontal,
+                start: "top 56px",
+                end: () => "+=" + horizontal.offsetWidth,
+                pin: true,
+                scrub: 1,
+                markers: false,
+                invalidateOnRefresh: true,
+                anticipatePin: 1,
             }
         })
 
-        return() =>{
-            scrollTween.kill()
-        }
-    },[])
+        return () => {
+            scrollTween.kill();
+        };
+    }, []);
 
 
 
     return (
-        <section id='port'>
+        <section id='port' ref={horizontalRef}>
             <div className='port_inner'>
                 <div className='port_title'>
                     portfolio<em>포폴 작업물</em>
                 </div>
                     <div className='port_wrap'>
                         {portText.map((port, key) => (
-                            <article className={`port_item p${key+1}`} key={key}>
+                            <article 
+                            className={`port_item p${key+1}`} 
+                            key={key}
+                            ref={(el) => {sectionRef.current[key] = el}}
+                            >
                                 <span className='num'>{port.num}.</span>
                                 <a href={port.code} target='_blank' className='img' rel='noreferrer'>
                                     <img src={port.img} alt={port.name} />
